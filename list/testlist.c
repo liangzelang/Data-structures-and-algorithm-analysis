1b7db4036b1fa80a5c89b3be9541d8d736066087
liangzelang <liangzelang@gmail.com>
2017-02-24 13:40:27 +0800

    singlelist

---
+++ list/testlist.c
@@ -0,0 +1,34 @@
+#include "list.h"
+
+int main()
+{
+	List L;
+	Position P;
+	int i;
+	
+	L = MakeEmpty( NULL);
+	P = Header(L);
+	PrintList(L);
+
+	for(i =0; i< 10; i++)
+	{
+		Insert(i, L ,P);
+		PrintList(L);
+		P = Advance(P);
+	}
+	for(i= 0;i<10;i+=2)
+	{
+		Delete(i,L);
+	}
+	for( i = 0; i < 10; i++ )
+        if( ( i % 2 == 0 ) == ( Find( i, L ) != NULL ) )
+            printf( "Find fails\n" );
+
+    printf( "Finished deletions\n" );
+
+    PrintList( L );
+
+    DeleteList( L );
+	system("pause");
+    return 0;
+}
